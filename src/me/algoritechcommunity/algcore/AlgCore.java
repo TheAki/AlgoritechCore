c73dace125cd521eeec4fbe62457a61d3e748d15
BuildTools <unconfigured@null.spigotmc.org>
2018-08-24 22:11:16 +0300

    Initial setup

---
+++ src/me/algoritechcommunity/algcore/AlgCore.java
@@ -0,0 +1,16 @@
+package me.algoritechcommunity.algcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AlgCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
