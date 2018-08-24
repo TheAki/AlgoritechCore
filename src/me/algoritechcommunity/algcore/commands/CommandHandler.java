ccf26c4b6e6a9d0056a6d7bf3180cc9e91229644
Akihiron <thedrunkracoon@gmail.com>
2018-08-24 23:06:02 +0300

    Basic command structure

--- src/me/algoritechcommunity/algcore/commands/CommandHandler.java
+++ src/me/algoritechcommunity/algcore/commands/CommandHandler.java
@@ -1,4 +1,22 @@
 package me.algoritechcommunity.algcore.commands;
 
-public class CommandHandler {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+
+public class CommandHandler implements CommandExecutor {
+
+
+    private final HashMap<String,CommandsAbstract>  COMMAND_OBJECTS = new HashMap<>();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(this.COMMAND_OBJECTS.containsKey(label)) {
+            return this.COMMAND_OBJECTS.get(label).onCommand(sender,command,label,args);
+        }
+        return false;
+    }
 }
--- src/me/algoritechcommunity/algcore/commands/CommandsAbstract.java
+++ src/me/algoritechcommunity/algcore/commands/CommandsAbstract.java
@@ -1,4 +1,46 @@
 package me.algoritechcommunity.algcore.commands;
 
+import me.algoritechcommunity.algcore.AlgCore;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+
 public abstract class CommandsAbstract {
+
+    private final AlgCore PLUGIN;
+    private final String LABEL;
+    private final String[] ALIASES;
+    private final HashMap<String, CommandsAbstract> COMMAND_OBJECTS;
+
+    public AlgCore getPlugin() {
+        return this.PLUGIN;
+    }
+
+    public String getLabel() {
+        return this.LABEL;
+    }
+
+    public String[] getAliases() {
+        return this.ALIASES;
+    }
+
+    public CommandsAbstract(AlgCore plugin, HashMap<String, CommandsAbstract> commandObjects, String label, String... aliases) {
+        this.PLUGIN = plugin;
+        this.LABEL = label;
+        this.ALIASES = aliases;
+        this.COMMAND_OBJECTS = commandObjects;
+
+        this.registerCommand();
+    }
+
+    private void registerCommand() {
+        this.COMMAND_OBJECTS.put(this.LABEL, this);
+
+        for(String alias : this.ALIASES) {
+            this.COMMAND_OBJECTS.put(alias,this);
+        }
+    }
+
+    abstract boolean onCommand(CommandSender sender, Command command, String label, String[] args);
 }
